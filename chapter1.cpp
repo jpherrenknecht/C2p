14f7160bc7ca55b7e1cc6fb6d24049236cbe96b2
jp.herrenknecht <jpherrenknecht@gmail.com>
2023-01-29 10:07:24 +0100

    Main

---
+++ chapter1.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+# include <string>
+int main()
+{ std::cout<<"Quel est votre nom? " ;
+std::string name;
+
+std::cin       >> name;
+
+std::cout <<"hello," <<name<< " !" <<std::endl;
+
+
+
+
+ }
