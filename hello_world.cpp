19caaa30a1e7f1101adb5681df85044f5c38d479
jp.herrenknecht <jpherrenknecht@gmail.com>
2023-01-29 10:07:32 +0100

    Main

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+
+{std::cout <<"Hello World \n\nCeci est un retour a la ligne"<<std::endl;
+std::cout <<"This is a (\") quote and this is (\\)slash"<<std::endl;
+
+std::cout <<" essai de tab \t 5 \t 6 "<<std::endl;
+return 0;
+}
